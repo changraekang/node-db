1381b1916c4c66736494fd2b00be039e159e02b8
changrae kang <89755609+changraekang@users.noreply.github.com>
2023-10-10 19:03:41 +0900

    Update db.js

--- db.js
+++ db.js
@@ -11,25 +11,10 @@ const dbConfig = {
   queueLimit: 0
 };
 
-// 환경 변수 유효성 검사
 if (!dbConfig.host || !dbConfig.user || !dbConfig.password || !dbConfig.database) {
   throw new Error('Database connection details are missing from environment variables.');
 }
 
-let pool;
+const pool = mysql.createPool(dbConfig);
 
-const connectDb = async () => {
-  try {
-    pool = mysql.createPool(dbConfig);
-    const conn = await pool.getConnection();
-    console.log("MySQL Connect Success.");
-    conn.release(); // 연결 테스트 후 연결을 반환
-  } catch (error) {
-    console.error("MySQL Connect Error:", error);
-    setTimeout(connectDb, 2000); // 재시도
-  }
-};
-
-connectDb();
-
-module.exports = pool.promise(); // 연결 풀 반환
+module.exports = pool;
