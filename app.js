9609d98a157ec0544d50f80727434179b16c4079
changrae kang <89755609+changraekang@users.noreply.github.com>
2023-10-10 18:19:23 +0900

    DB에저장 app.js

--- app.js
+++ app.js
@@ -27,19 +27,38 @@ async function save() {
         time.setSeconds(now.getSeconds() + i);
         const value = time.toISOString().split('T')[1].split('.')[0];
 
-        await client.rpush('covi-back', value);
+        await client.rPush('covi-back', value);
 
-        const length = await client.llen('covi-back');
+        const length = await client.lLen('covi-back');
         if (length === 1000) {
-            const items = await client.lrange('covi-back', 0, 999); // 처음부터 999번째 아이템까지 가져옵니다.
-            await client.rpush('covi-back-array', JSON.stringify(items)); // 배열을 문자열로 변환하여 저장
-            await client.ltrim('covi-back', 1000, -1); // 처음 1000개 아이템 제거
+            const items = await client.lRange('covi-back', 0, 999); // 처음부터 999번째 아이템까지 가져옵니다.
+            await client.rPush('covi-back-array', JSON.stringify(items)); // 배열을 문자열로 변환하여 저장
+            await client.lTrim('covi-back', 1000, -1); // 처음 1000개 아이템 제거
+             // DB에 저장
+        await insertToDB(items);
         }
+        
     }
 
     // 리스트 추가 완료 후 Redis 연결 종료
     client.quit();
 }
+
+async function insertToDB(items) {
+    const query = "INSERT INTO covision (covisionName) VALUES (?)";
+
+    for (const item of items) {
+        try {
+            await db.query(query, [item]); // db.js에서 query 기능이 제공되는 것으로 가정
+        } catch (error) {
+            console.error("Error inserting into database:", error);
+        }
+    }
+
+    // 데이터를 DB에 저장한 후, Redis에서 해당 데이터 삭제
+    await client.lTrim('covi-back-array', items.length, -1);
+}
+
 
 // run 함수 호출로 프로그램 시작
 run();
